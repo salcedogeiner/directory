98424610864739ce4a59a48742293e4788f5788f
salcedogeiner <geiner.salcedo.ppc0111@gmail.com>
2016-08-17 22:22:42 -0500

    permissions admin

--- app/admin/person.rb
+++ app/admin/person.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register Person do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+ permit_params :first_name, :last_name, :tel, :email
 #
 # or
 #
